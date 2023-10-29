998f31d927c7ac3b45b96629ecdced21d47ba493
Mateo Mohr <mohr.mateo@gmail.com>
2023-10-29 16:41:58 -0300

    ready for deploy

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-
-
+import { BrowserRouter } from 'react-router-dom'; 
 import { Provider } from "react-redux";
 
 import axios from 'axios'; 
 
 import  store  from "./redux/store";
 
-axios.defaults.baseURL = 'https://smart-api-utjh.onrender.com'
+axios.defaults.baseURL = 'http://54.165.140.58/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
